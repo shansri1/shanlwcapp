7b8ecf0ed8d59fc3b7bbb608b4b5db1bb61081ac
shansri1 <sveerap@hotmail.com>
2021-06-17 21:45:07 +0530

    yes2

--- .history/src/client/modules/my/app/app_20210617203106.js
+++ .history/src/client/modules/my/app/app_20210617203106.js
@@ -2,10 +2,7 @@ import { LightningElement } from 'lwc';
 export default class App extends LightningElement {
   sessionId;
 
-  state = 'list';
-
   handleNavigate(event) {
-    this.state = event.detail.state;
     this.sessionId = event.detail.sessionId;
   }
 }
